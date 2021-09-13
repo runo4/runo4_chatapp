9e83db4c28ca64ae47533bc2709bbb25598305eb
runo4 <runo1218@gmail.com>
2021-09-13 20:08:29 +0900

    composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '463fae0e2bbd633d69d49affe0b481826a433915',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '463fae0e2bbd633d69d49affe0b481826a433915',
+            'dev_requirement' => false,
+        ),
+    ),
+);
