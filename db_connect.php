4cdd8c4a3bf650678a306e35f744d5b736416056
runo4 <runo1218@gmail.com>
2021-09-13 20:25:57 +0900

    fixed

--- db_connect.php
+++ db_connect.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_USERNAME', 'b8066086384ba2');
 define('DB_PASSWORD', '0a7497aa');
-define('DSN', 'mysql:host=us-cdbr-east-04.cleardb.com; dbname=php_testdb; charset=utf8');
+define('DSN', 'mysql:host=us-cdbr-east-04.cleardb.com; dbname=heroku_a7e8affb5d79370; charset=utf8');
 
 function db_connect(){
     $pdo = new PDO(DSN, DB_USERNAME, DB_PASSWORD);
